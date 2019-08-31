9ee89f5bca1d2f1d8ddddf12beeab631a01aec44
mustafa sarıel <mustafasariel@hotmail.com>
2019-08-31 17:29:38 +0300

    task awit async code trainning

--- taskDemo/Program.cs
+++ taskDemo/Program.cs
@@ -12,11 +12,14 @@ namespace taskDemo
         static async Task Main(string[] args)
         {
             white();
-            _ = red();
-            _ = yellow();
-            _ = black();
+            Task tred = red();
+            Task tyellow = yellow();
+            Task tblack = black();
 
+            await Task.WhenAll(tred, tyellow, tblack);
+            green();
             Console.ReadKey();
+
         }
 
         public static async Task red()
@@ -60,6 +63,15 @@ namespace taskDemo
                 Console.WriteLine($"{i} white");
             }
             Console.WriteLine("white --------------end");
+        }
+        public static void green()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine($"{i} green");
+            }
+            Console.WriteLine("green --------------end");
         }
 
         public static async Task black()
