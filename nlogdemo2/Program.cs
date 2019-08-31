605044dce605af246a5b389a1ce44d1098ca900a
mustafa sarıel <mustafasariel@hotmail.com>
2019-09-01 01:12:59 +0300

    nlog demo

---
+++ nlogdemo2/Program.cs
@@ -0,0 +1,30 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nlogdemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = NLog.LogManager.GetLogger("test");
+            // Trace - Warn levelları arasında FileManager2'yi kullanacak.
+            logger.Log(LogLevel.Info, "Sample Info error message");
+            logger.Log(LogLevel.Debug, "Sample Debug");
+            logger.Log(LogLevel.Warn, "Sample Warn");
+            logger.Log(LogLevel.Trace, "Sample Trace");
+            //Error ve yukarısı için FileManager'ı kullanacak.
+            logger.Log(LogLevel.Error, new Exception("Hata1"));
+            logger.Log(LogLevel.Error, new Exception("Hata2"));
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
