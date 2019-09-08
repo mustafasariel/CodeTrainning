12ebe6120bdcf507f1170861a0a4a5afd4b44dca
mustafa sarıel <mustafasariel@hotmail.com>
2019-09-09 01:15:27 +0300

    lock demo

---
+++ lockDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class AccountTest
+{
+    static void Main()
+    {
+        var account = new Account(1000);
+        var tasks = new Task[100];
+        for (int i = 0; i < tasks.Length; i++)
+        {
+            tasks[i] = Task.Run(() => RandomlyUpdate(account));
+        }
+        Task.WaitAll(tasks);
+    }
+
+    static void RandomlyUpdate(Account account)
+    {
+        var rnd = new Random();
+        for (int i = 0; i < 10; i++)
+        {
+            var amount = rnd.Next(1, 100);
+            bool doCredit = rnd.NextDouble() < 0.5;
+            if (doCredit)
+            {
+                account.Credit(amount);
+            }
+            else
+            {
+                account.Debit(amount);
+            }
+        }
+    }
+}
