db9d13bc47cc5cd91e5274e9f827a05757072370
mustafa sarıel <mustafasariel@hotmail.com>
2019-10-06 21:41:08 +0300

    lockDemo

---
+++ lockDemo2/Ornek.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace lockDemo2
+{
+    public class Ornek
+    {
+
+        private readonly object _kilit = new object();
+
+        readonly List<int> _genericList = new List<int>();
+
+        private readonly int _basilacakDegerSayisi = 0;
+
+        public Ornek(int baslanilacakDegerSayisi)
+        {
+            _basilacakDegerSayisi = baslanilacakDegerSayisi;
+        }
+
+        public void VerileriBas()
+        {
+            lock (_kilit)
+            {
+                for (var i = 0; i < _basilacakDegerSayisi; i++)
+                {
+                    _genericList.Add(i);
+                    Thread.Sleep(1000);
+                    Console.WriteLine(i+ "ekledi");
+                }
+                Console.WriteLine("Döngü Tamamlandı!");
+            }
+        }
+
+        public void VeriSil(int silinecekVerininIndexNumarasi)
+        {
+            //kullanıcı eğer silinecek numarayı girerse ve eğer
+            //_kilit isimli LOCK işlem için dondurulmuşsa burada beklemeye
+            //alınacaktır. Diğer işlem bitimine kadar Lock içindeki kodlar çalışmayacaktır.
+            lock (_kilit)
+            {
+                Console.WriteLine("VeriSil Metodu Çalıştı.");
+                _genericList.RemoveAt(silinecekVerininIndexNumarasi);
+                Console.WriteLine($"Liste {_genericList.Count}");
+            }
+           
+        }
+
+        public void BastanSil()
+        {
+           
+            lock (_kilit)
+            {
+                while (_genericList.Count > 0)
+                {
+
+                    _genericList.RemoveAt(0);
+
+                    Console.WriteLine("Silme sonrası");
+                    foreach (var item in _genericList)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    Console.WriteLine("-------------------");
+                    Thread.Sleep(1000);
+
+                }
+            }
+        }
+        public void Katla()
+        {
+            lock (_kilit)
+            {
+                for (int i = 0; i < _genericList.Count; i++)
+                {
+                    _genericList[i] = _genericList[i] * _genericList[i];
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
---
+++ lockDemo2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lockDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adet:");
+            int adet = int.Parse(Console.ReadLine());
+
+            var ornek = new Ornek(adet);
+
+            var threadEkle = new Thread(ornek.VerileriBas);
+
+            threadEkle.Start();
+
+
+            var threadSil = new Thread(ornek.BastanSil);
+
+            threadSil.Start();
+
+
+
+            var threadKatla = new Thread(ornek.Katla);
+
+            threadKatla.Start();
+        }
+    }
+}
